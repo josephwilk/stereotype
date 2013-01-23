369825827c232654605be8759ffcc4e57927e3f1
josephwilk <joe@josephwilk.net>
2013-01-23 20:14:03 +0100

    WIP: Shape out API Failing tests telling me what to do next!

--- src/stereotype_clj/core.clj
+++ src/stereotype_clj/core.clj
@@ -1,1 +1,10 @@
 (ns stereotype-clj.core)
+
+(defmacro defsterotype
+  "define a sterotype with default attributes"
+  [name attributes] attributes)
+
+(defn sterotype
+  "returns the sterotype defaults"
+  [name & overiding_attributes]
+  {:name "josephwilk" :company "soundcloud"})
--- test/stereotype_clj/unit/t_core.clj
+++ test/stereotype_clj/unit/t_core.clj
@@ -2,3 +2,12 @@
   (:use
     [midje.sweet :refer :all]
     [stereotype-clj.core]))
+
+(facts "defining defaults for a sterotype"
+  (fact "it should return a map with default values"
+    (defsterotype :user {:name "josephwilk"})
+    (sterotype :user) => {:name "josephwilk"})
+
+  (fact "it should return a map with default values"
+    (defsterotype :user {:name "josephwilk" :company "soundcloud"})
+    (sterotype :user {:name "monkey"}) => {:name "monkey" :company "soundcloud"}))
