6321df1bc0230a90c9ffe55dce0af1316652cad8
josephwilk <joe@josephwilk.net>
2013-01-24 13:56:01 +0100

    WIP: Actually store entities by name. Using evil shared state. Shhh

--- src/stereotype_clj/core.clj
+++ src/stereotype_clj/core.clj
@@ -1,10 +1,17 @@
 (ns stereotype-clj.core)
 
+(def sterotypes (atom {}))
+
+(defn update-sterotypes [new-sterotype]
+  (reset! sterotypes (merge @sterotypes new-sterotype)))
+
 (defmacro defsterotype
   "define a sterotype with default attributes"
-  [name attributes] attributes)
+  [name attributes]
+  (update-sterotypes {name attributes})
+  (println @sterotypes))
 
 (defn sterotype
   "returns the sterotype defaults"
   [name & [overiding_attributes]]
-  (merge {:name "josephwilk" :company "soundcloud"} overiding_attributes))
+  (merge (name @sterotypes) overiding_attributes))
--- test/stereotype_clj/unit/t_core.clj
+++ test/stereotype_clj/unit/t_core.clj
@@ -3,11 +3,17 @@
     [midje.sweet :refer :all]
     [stereotype-clj.core]))
 
+(background (before :facts (update-sterotypes {})))
+
 (facts "defining defaults for a sterotype"
-  (fact "it should return a map with default values"
+  (fact "it should return the user entity defaults"
     (defsterotype :user {:name "josephwilk"})
     (sterotype :user) => {:name "josephwilk"})
 
-  (fact "it should return a map with default values"
+  (fact "it should return the monkey entity defaults"
+    (defsterotype :monkey {:name "baboon"})
+    (sterotype :monkey) => {:name "baboon"})
+
+  (fact "it should overide defaults when they are specified"
     (defsterotype :user {:name "josephwilk" :company "soundcloud"})
     (sterotype :user {:name "monkey"}) => {:name "monkey" :company "soundcloud"}))
