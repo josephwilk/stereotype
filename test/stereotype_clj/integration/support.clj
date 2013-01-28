c8a755bbc5f5932938a42d403c1435b581553ac7
josephwilk <joe@josephwilk.net>
2013-01-28 12:52:03 +0100

    Factor out db support setup. Try and make the integration test more like how the lib will be used by clients.

---
+++ test/stereotype_clj/integration/support.clj
@@ -0,0 +1,11 @@
+(ns stereotype-clj.integration.support
+  (:require
+    [korma.core          :refer :all]
+    [korma.db            :refer :all]
+    [clojure.java.jdbc :as sql]
+    [stereotype-clj.core :refer :all]))
+
+(defdb mydb {:classname "org.sqlite.JDBC"
+             :subprotocol "sqlite"
+             :subname "db/test.sqlite3"})
+(exec-raw ["CREATE TABLE IF NOT EXISTS users(username TEXT);"])
--- test/stereotype_clj/integration/t_core.clj
+++ test/stereotype_clj/integration/t_core.clj
@@ -2,22 +2,15 @@
   (:require
     [midje.sweet :refer :all]
     [stereotype-clj.core :refer :all]
-    [clojure.java.jdbc :as sql]
-    [korma.db   :refer :all]
+    [stereotype-clj.integration.support :refer :all]
     [korma.core :refer :all]))
 
-(defdb mydb {:classname "org.sqlite.JDBC"
-             :subprotocol "sqlite"
-             :subname "db/test.sqlite3"})
-
-(exec-raw ["CREATE TABLE IF NOT EXISTS users(username TEXT);"])
-
 (defentity users)
+(defsterotype :users {:username "josephwilk"})
 
 (background (before :contents (exec-raw ["DELETE FROM users;"])))
     
 (facts "sterotype!"
   (fact "it should create a record in the database with default values"
-    (defsterotype :users {:username "josephwilk"})
     (sterotype! :users)
     (first (select users)) => {:username "josephwilk"}))
