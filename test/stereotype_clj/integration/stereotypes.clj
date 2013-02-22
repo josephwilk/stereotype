8054bb657fdb676b937d7540fd84140ac31223ab
josephwilk <joe@josephwilk.net>
2013-02-22 14:20:46 +0100

    typo

--- test/stereotype_clj/integration/stereotypes.clj
+++ test/stereotype_clj/integration/stereotypes.clj
@@ -1,7 +1,7 @@
 (ns stereotype-clj.integration.stereotypes
   (:require
     [stereotype-clj.core :refer :all]
-    [clj-time.core       :as :time]))
+    [clj-time.core       :as time]))
 
 (defstereotype :users {:username "josephwilk"
                        :date_of_birth #(time/now)
