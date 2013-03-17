f8de9f9c50923357c7f8bae5d8ee73b4875bf287
Joseph Wilk <joe@josephwilk.net>
2013-03-17 12:46:56 +0100

    Ensure we get an error on referencing a undefined stereotype

--- test/stereotype_clj/unit/t_core.clj
+++ test/stereotype_clj/unit/t_core.clj
@@ -16,7 +16,10 @@
 
   (fact "it should overide defaults when they are specified"
     (defstereotype :employee {:name "josephwilk" :company "soundcloud"})
-    (stereotype :employee {:name "monkey"}) => {:name "monkey" :company "soundcloud"}))
+    (stereotype :employee {:name "monkey"}) => {:name "monkey" :company "soundcloud"})
+
+  (fact "it should raise an error on referencing an undefined stereotype"
+    (stereotype :something-made-up) => (throws Exception #":something-made-up not found")))
 
 (facts "sequences"
   (fact "it should generate unique emails"
