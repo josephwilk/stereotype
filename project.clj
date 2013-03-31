7a16c190cb81e3705a62bdedde064bdf19ac5521
Joseph Wilk <joe@josephwilk.net>
2013-03-31 19:21:41 +0200

    cut 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stereotype "0.2.0"
+(defproject stereotype "0.2.1"
   :description "A library for setting up test data in Clojure "
   :url "https://github.com/josephwilk/stereotype"
   :license {:name "MIT License"
@@ -9,7 +9,7 @@
 
   :min-lein-version "2.0.0"
   :profiles {:shared {:plugins [[lein-midje "3.0-beta1"]]}
-             :dev [:shared {:dependencies [                                   [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [org.xerial/sqlite-jdbc "3.7.2"] [clj-time "0.4.4"]]}]
-             :1.3 [:shared {:dependencies [[org.clojure/clojure "1.3.0"]      [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [org.xerial/sqlite-jdbc "3.7.2"] [clj-time "0.4.4"]]}]
-             :1.4 [:shared {:dependencies [[org.clojure/clojure "1.4.0"]      [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [org.xerial/sqlite-jdbc "3.7.2"] [clj-time "0.4.4"]]}]
-             :1.5 [:shared {:dependencies [[org.clojure/clojure "1.5.1"]      [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [org.xerial/sqlite-jdbc "3.7.2"] [clj-time "0.4.4"]]}]})
+             :dev [:shared {:dependencies [                                   [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [postgresql/postgresql "8.4-702.jdbc4"] [clj-time "0.4.4"]]}]
+             :1.3 [:shared {:dependencies [[org.clojure/clojure "1.3.0"]      [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [postgresql/postgresql "8.4-702.jdbc4"] [clj-time "0.4.4"]]}]
+             :1.4 [:shared {:dependencies [[org.clojure/clojure "1.4.0"]      [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [postgresql/postgresql "8.4-702.jdbc4"] [clj-time "0.4.4"]]}]
+             :1.5 [:shared {:dependencies [[org.clojure/clojure "1.5.1"]      [midje "1.5.1"] [org.clojure/java.jdbc "0.2.3"] [postgresql/postgresql "8.4-702.jdbc4"] [clj-time "0.4.4"]]}]})
