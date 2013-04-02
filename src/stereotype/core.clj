48d30266f9bb5a7efe3d27aab96e09d8b1cee795
josephwilk <joe@josephwilk.net>
2013-04-02 13:07:40 +0200

    Revert "Experiment with moving scope into stereotypes"

--- src/stereotype/core.clj
+++ src/stereotype/core.clj
@@ -6,7 +6,9 @@
 (defmacro defstereotype
   "define a stereotype with default attributes"
   [stereotype-id attributes]
-  (stereotypes/define stereotype-id attributes))
+  `(defn ~(stereotypes/fn-name stereotype-id) [overiding-attributes#]
+     (apply (stereotypes/stereotype-fn ~attributes)
+            [overiding-attributes#])))
 
 (defn stereotype
   "returns the stereotype defaults"
